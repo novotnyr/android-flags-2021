1c4f03f0b120c703c8e197144a740349ab0775c4
Robert Novotny <novotnr0@gmail.com>
2021-03-03 23:35:18 +0100

    Pridana kostra master aktivity (zoznam)

---
+++ app/src/main/java/com/github/novotnyr/flags/FlagListActivity.kt
@@ -0,0 +1,11 @@
+package com.github.novotnyr.flags
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FlagListActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_flag_list)
+    }
+}
