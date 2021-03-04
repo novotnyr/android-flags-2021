1df7d8dea87ed3a9ffbdafac96984e36a7315072
Robert Novotny <novotnr0@gmail.com>
2021-03-04 01:29:07 +0100

    Pridany zaklad pre viewholder

---
+++ app/src/main/java/com/github/novotnyr/flags/FlagViewHolder.kt
@@ -0,0 +1,13 @@
+package com.github.novotnyr.flags
+
+import android.view.View
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class FlagViewHolder(layout: View) : RecyclerView.ViewHolder(layout) {
+    val textView: TextView = layout.findViewById(android.R.id.text1)
+
+    fun bind(flag: Flag) {
+        textView.text = flag.country
+    }
+}
