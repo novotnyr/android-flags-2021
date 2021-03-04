721bd01cad0266a18ca8a2eb3b3e68ebe176a550
Robert Novotny <novotnr0@gmail.com>
2021-03-05 00:15:46 +0100

    Pridana detailova aktivita

---
+++ app/src/main/java/com/github/novotnyr/flags/FlagDetailActivity.kt
@@ -0,0 +1,11 @@
+package com.github.novotnyr.flags
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FlagDetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_flag_detail)
+    }
+}
