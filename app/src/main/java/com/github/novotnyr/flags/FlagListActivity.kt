c2f91fa446f6b79d3d45daaa3deacd8bf7711446
Robert Novotny <novotnr0@gmail.com>
2021-03-04 01:27:52 +0100

    Pridany RecyclerView do layoutu

--- app/src/main/java/com/github/novotnyr/flags/FlagListActivity.kt
+++ app/src/main/java/com/github/novotnyr/flags/FlagListActivity.kt
@@ -2,10 +2,17 @@ package com.github.novotnyr.flags
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class FlagListActivity : AppCompatActivity() {
+    lateinit var flagsRecyclerView: RecyclerView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_flag_list)
+
+        flagsRecyclerView = findViewById(R.id.flagsRecyclerView)
+        flagsRecyclerView.layoutManager = LinearLayoutManager(this)
     }
 }
