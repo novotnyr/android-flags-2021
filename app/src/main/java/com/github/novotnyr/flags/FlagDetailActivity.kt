1ec47296b029ddf7cffd2002f25f1d0a0f388036
Robert Novotny <novotnr0@gmail.com>
2021-03-05 16:41:18 +0100

    Pridanie ImageView na zobrazenie vlajky

--- app/src/main/java/com/github/novotnyr/flags/FlagDetailActivity.kt
+++ app/src/main/java/com/github/novotnyr/flags/FlagDetailActivity.kt
@@ -2,10 +2,15 @@ package com.github.novotnyr.flags
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
 
 class FlagDetailActivity : AppCompatActivity() {
+    private lateinit var flagImageView: ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_flag_detail)
+
+        flagImageView = findViewById(R.id.flagImageView)
     }
 }
